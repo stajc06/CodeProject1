d59f6beace4b036139011c5fc8771e71861813d1
David <stdnd19@gmail.com>
2017-02-10 12:34:35 -0500

    Durski duck and Team2Flock

---
+++ team2Flock.java
@@ -0,0 +1,9 @@
+package headfirst.strategy;
+public class team2Flock {
+    DurskiDuck durski = new DurskiDuck();
+
+        public void team2Flock() {
+            durski.display();
+            durski.performQuack();
+        }
+    }
