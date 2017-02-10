6cab1398c4aceff609ff388dbf57b57ef0829f8d
Rocco <strtf01@moravian.edu>
2017-02-10 12:44:33 -0500

    duck push

--- team2Flock.java
+++ team2Flock.java
@@ -2,12 +2,13 @@ package headfirst.strategy;
 public class team2Flock {
     DurskiDuck durski = new DurskiDuck();
     PolichDuck polich = new PolichDuck();
-
+    FatuovaDuck fatuova = new FatuovaDuck();
         public void team2Flock() {
             durski.display();
             durski.performQuack();
             polich.display();
             polich.performQuack();
-
+            fatuova.display();
+            fatuova.performQuack();
         }
     }
