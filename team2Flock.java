3fcca18901a6ac7ce6560bbcf7bd1a64488e8819
John Polich <stjnp20@moravian.edu>
2017-02-10 12:39:16 -0500

    polich duck

--- team2Flock.java
+++ team2Flock.java
@@ -1,9 +1,13 @@
 package headfirst.strategy;
 public class team2Flock {
     DurskiDuck durski = new DurskiDuck();
+    PolichDuck polich = new PolichDuck();
 
         public void team2Flock() {
             durski.display();
             durski.performQuack();
+            polich.display();
+            polich.performQuack();
+
         }
     }
