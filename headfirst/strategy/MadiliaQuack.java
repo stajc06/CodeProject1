9cfbd30b057d82e5dafc64683ff0b7e1e81da087
Mario <stmos01@moravian.edu>
2017-02-15 12:12:08 -0500

    add

---
+++ headfirst/strategy/MadiliaQuack.java
@@ -0,0 +1,10 @@
+package headfirst.strategy;
+
+/**
+ * Created by justinmadilia on 2/10/17.
+ */
+public class MadiliaQuack implements QuackBehavior {
+    public void quack() {
+        System.out.println("Quacks like a Madilia");
+    }
+}
