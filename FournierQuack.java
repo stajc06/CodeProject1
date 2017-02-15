b372323c8680558951e6c79286ee980954a436bc
murphyt <sttam09@moravian.edu>
2017-02-15 12:09:34 -0500

    All ducks

---
+++ FournierQuack.java
@@ -0,0 +1,10 @@
+package headfirst.strategy;
+
+/**
+ * Created by cFournierg on 2/10/17.
+ */
+public class FournierQuack implements QuackBehavior {
+    public void quack() {
+        System.out.println("Bark Bark");
+    }
+}
