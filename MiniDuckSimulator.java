d06489c5f03ee0bebcbf317ef7b7a16391ee3c87
Andrew <stajc06@moravian.edu>
2017-02-20 13:22:53 -0500

    try 3

---
+++ MiniDuckSimulator.java
@@ -0,0 +1,26 @@
+
+package headfirst.strategy;
+
+public class MiniDuckSimulator {
+
+<<<<<<< HEAD
+        CarrFlock carr = new CarrFlock();
+        //Team1Flock = new team1();
+=======
+    public static void main(String[] args) {
+
+
+    
+>>>>>>> e0d24e3dd1e56251bcc36e5aefc9d1f429be8d45
+
+        
+
+<<<<<<< HEAD
+        carr.CarrFlock();
+		//team1.Team1Flock();
+=======
+>>>>>>> e0d24e3dd1e56251bcc36e5aefc9d1f429be8d45
+
+
+    }
+}
